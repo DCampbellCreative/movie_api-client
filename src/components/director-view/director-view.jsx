79b1073f406620e84c64ed601f3070136717de1d
DCampbellCreative <dougcampbell1990@gmail.com>
2021-11-01 13:55:51 -0400

    added director view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+
+export class DirectorView extends React.Component {
+ 
+  render() {
+    const { movie, onBackClick } = this.props;
+    
+    return (   
+      <Row>
+        
+          
+        
+    <Card className="director-view">
+    <Card.Title>
+        <span className="label">Name: </span>
+        <span className="value">{movie.director.Name}</span>
+      </Card.Title>
+       <Card.Body>
+        <span className="label">Description: </span>
+        <span className="value">{movie.director.Bio}</span>
+       </Card.Body>
+      </Card>
+      
+      </Row>
+      );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+   Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
